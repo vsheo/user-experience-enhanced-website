8a988114cf86c59392d18bb7f949b5ae679a2115
vsheo <vireshsheoratan@gmail.com>
2025-05-07 09:34:56 +0200

    anchor loader uit #30

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,30 +1,30 @@
 // loading bar als een link geklikt wordt
-const loading = document.querySelector(".loading-overlay");
-const anchorTags = document.querySelectorAll("a");
+// const loading = document.querySelector(".loading-overlay");
+// const anchorTags = document.querySelectorAll("a");
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    if (loading.classList.contains("hidden")) {
-        // op elke link is er een event istener
-        anchorTags.forEach((link) => {
-            // bij een klik komt de loading circle op beeld
-            link.addEventListener("click", () => {
-                loading.classList.remove("hidden");
-            });
-        });
-    }
-    else {
-        loading.classList.add("hidden");
-    }
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     if (loading.classList.contains("hidden")) {
+//         // op elke link is er een event istener
+//         anchorTags.forEach((link) => {
+//             // bij een klik komt de loading circle op beeld
+//             link.addEventListener("click", () => {
+//                 loading.classList.remove("hidden");
+//             });
+//         });
+//     }
+//     else {
+//         loading.classList.add("hidden");
+//     }
+// });
 
 // De loading circle blijft op scherm beeld als de back button geklikt wordt
 // reset wanneer de website ingeladen wordt
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/pageshow_event
 // https://stackoverflow.com/questions/15640087/javascript-unchecking-all-checkboxes
-window.addEventListener("pageshow", () => {
-    loading.classList.add("hidden");
-});
+// window.addEventListener("pageshow", () => {
+//     loading.classList.add("hidden");
+// });
 
 
 // back button
